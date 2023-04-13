e55fed07feed2ed73ff6c3e64781f7ad08578445
DS <mc_sd@bk.ru>
2023-04-12 22:08:47 -0300

    delete git

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int myMiles;smyMiles = price / 20;
+        int myMiles;myMiles = price / 20;
         return myMiles;
 
 
